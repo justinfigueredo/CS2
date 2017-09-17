3d3e949b46dddb7341e4d8f3b7ced8b28c8d0380
justinfigueredo <31266437+justinfigueredo@users.noreply.github.com>
2017-09-17 15:00:06 -0600

    Add files via upload

---
+++ HW2/HW2/src/CountriesLL.java
@@ -0,0 +1,24 @@
+
+public class CountriesLL {
+	
+	String countryName = "";
+	String latitude = "";
+	String longitude = "";
+	int countryArea = 0;
+	int countryPopulation = 0;
+	double countryGDP = 0.0; // in billions
+	int countryYear = 0;
+	CountriesLL next;
+
+	public CountriesLL(String name, String lat, String lon, int area, int pop, double gdp, int year)
+	{
+		countryName = name;
+		latitude = lat;
+		longitude = lon;
+		countryArea = area;
+		countryPopulation = pop;
+		countryGDP = gdp;
+		countryYear = year;
+	}
+	
+}
