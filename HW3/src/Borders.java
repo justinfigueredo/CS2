0ee85ca2276fbf01e01344c8de52b46129ab878f
justinfigueredo <31266437+justinfigueredo@users.noreply.github.com>
2017-09-26 23:00:54 -0600

    Add files via upload

--- HW3/src/Borders.java
+++ HW3/src/Borders.java
@@ -1,39 +1,14 @@
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+
 import java.util.ArrayList;
 
 public class Borders {
 	
-public String[][] borders = new String[16][2];
-
-	public void importBordersData() throws IOException
+	public Borders()
 	{
-		String data = "";
-		int count = 0;
-			try {
-				BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\Justin\\workspace\\HW2\\Borders.txt"));
-				while ((data = br.readLine()) != null) { // makes data equal to the next line and checks if it is null
-					String[] parts = data.split(","); // splits the read in line into an array
-					borders[count][0] = parts[0]; //
-					borders[count][1] = parts[1]; //fills the 2d array
-					count++;
-				}
-				br.close();
-				
-//				for(int i = 0; i< borders.length; i++)   I used this to test
-//				{
-//					System.out.println(borders[i][0] + " " + borders[i][1]);
-//				}
-				
-			} catch (FileNotFoundException e) {
-				
-				e.printStackTrace();
-			}
+	
 	}
 	
-	public ArrayList<String> getCountriesThatBorder(String country)
+	public ArrayList<String> getCountriesThatBorder(String country, String [][] borders)
 	{
 
 		ArrayList<String> borderingCountries = new ArrayList<String>();
--- HW3/src/HW3.java
+++ HW3/src/HW3.java
@@ -16,6 +16,7 @@ public class HW3 {
 public ArrayList<Countries> countryList = new ArrayList<Countries>();
 public LinkedList<CountriesLL> countryLL;
 public boolean done = false;
+public String[][] borders = new String[16][2];
 
 	public static void main(String[] args) throws IOException {
 		try
@@ -71,6 +72,31 @@ public boolean done = false;
 				e.printStackTrace();
 			}
 	}
+
+	public void importBordersData() throws IOException
+	{
+		String data = "";
+		int count = 0;
+			try {
+				BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\Justin\\workspace\\HW2\\Borders.txt"));
+				while ((data = br.readLine()) != null) { // makes data equal to the next line and checks if it is null
+					String[] parts = data.split(","); // splits the read in line into an array
+					borders[count][0] = parts[0]; //
+					borders[count][1] = parts[1]; //fills the 2d array
+					count++;
+				}
+				br.close();
+				
+//				for(int i = 0; i< borders.length; i++)   I used this to test
+//				{
+//					System.out.println(borders[i][0] + " " + borders[i][1]);
+//				}
+				
+			} catch (FileNotFoundException e) {
+				
+				e.printStackTrace();
+			}
+	}
 	
 	public ArrayList<String> getCountriesWithPopulation(int pop)
 	{
@@ -112,7 +138,7 @@ public boolean done = false;
 	public void displayImportData(Borders border, HW3 thing) throws IOException
 	{
 		System.out.println("Importing Data"); 
-		border.importBordersData(); //import data
+		thing.importBordersData(); //import data
 		thing.importCountriesData(); //import data
 	}
 	
@@ -125,7 +151,7 @@ public boolean done = false;
 		{
 			System.out.print("What country would you like to see the bordering countries for? (first letter capitalized please) ");
 			input = keyboard.nextLine();
-			ArrayList<String> countries = border.getCountriesThatBorder(input); // calls method that returns the bordering countries of the country given
+			ArrayList<String> countries = border.getCountriesThatBorder(input, borders); // calls method that returns the bordering countries of the country given
 			if(countries.size() == 0) //makes sure there actually are countries in the list
 				System.out.print("No countries border " + input + "\n");
 			else
@@ -179,7 +205,7 @@ public boolean done = false;
 			System.out.print("What number would you like to make the population? ");
 			int pop = keyboard.nextInt();
 			System.out.println("(ArrayList implementation)"); // array implementation of getting the countries with a population greater than the given number
-			ArrayList<String> countries = thing.getCommonCountries(thing.getCountriesWithPopulation(pop),border.getCountriesThatBorder(input));
+			ArrayList<String> countries = thing.getCommonCountries(thing.getCountriesWithPopulation(pop),border.getCountriesThatBorder(input, borders));
 			if(countries.size() == 0) //makes sure there actually are countries in the list
 				System.out.print("No countries have a population greater than " + pop + " and border " + input + "\n");
 			else
@@ -189,7 +215,7 @@ public boolean done = false;
 			}
 			
 			System.out.println("(Linked List implementation)"); // linked list implementation of getting the countries with a population less than the given number
-			ArrayList<String> countriesLL = thing.getCommonCountries(thing.getCountriesWithPopulationLL(pop),border.getCountriesThatBorder(input));
+			ArrayList<String> countriesLL = thing.getCommonCountries(thing.getCountriesWithPopulationLL(pop),border.getCountriesThatBorder(input, borders));
 			if(countriesLL.size() == 0) //makes sure there actually are countries in the list
 				System.out.print("No countries have a population greater than " + pop + " and border " + input + "\n");
 			else
